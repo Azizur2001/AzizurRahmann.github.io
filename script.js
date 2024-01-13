cbe8aaded8d8c78e9c8218b2eebd8fbb877b0d9a
Azizur2001 <81169211+Azizur2001@users.noreply.github.com>
2024-01-12 23:39:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function showNavigatorInfo() {
+    var info = 'Navigator:\n';
+    info += 'appName: ' + navigator.appName + '\n';
+    info += 'product: ' + navigator.product + '\n';
+    info += 'appVersion: ' + navigator.appVersion + '\n';
+    info += 'userAgent: ' + navigator.userAgent + '\n';
+    info += 'platform: ' + navigator.platform + '\n';
+    info += 'language: ' + navigator.language;
+    alert(info);
+}
+
+function showWindowInfo() {
+
+}
+
+function showScreenInfo() {
+
+}
+
+function showLocationInfo() {
+
+}
+
+function showGeologicalInfo() {
+
+}
